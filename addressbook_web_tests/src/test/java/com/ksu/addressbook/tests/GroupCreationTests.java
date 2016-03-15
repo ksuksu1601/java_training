78d9d20d47257595297d04a15a8b0637bd53b5f6
Ksenia Rybakova <ksuksu1601@gmail.com>
2016-03-16 01:32:00 +0300

    Change the way of getting max groupId (loop changed to stream max with lambda)

--- addressbook_web_tests/src/test/java/com/ksu/addressbook/tests/GroupCreationTests.java
+++ addressbook_web_tests/src/test/java/com/ksu/addressbook/tests/GroupCreationTests.java
@@ -4,8 +4,10 @@ import com.ksu.addressbook.model.GroupData;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
+import java.util.Comparator;
 import java.util.HashSet;
 import java.util.List;
+import java.util.zip.Inflater;
 
 public class GroupCreationTests extends TestBase{
 
@@ -19,11 +21,25 @@ public class GroupCreationTests extends TestBase{
         List<GroupData> after = app.getGroupHelper().getGroupList();
         Assert.assertEquals(after.size(), before.size() + 1);
 
-        int max = 0;
-        for (GroupData g: after){
-            max = g.getId() > max ? g.getId() : max;
-        }
-        group.setId(max);
+//        Way 1:
+//        int max = 0;
+//        for (GroupData g: after){
+//            max = g.getId() > max ? g.getId() : max;
+//        }
+
+//        Way 2:
+//        Comparator<? super GroupData> byId = new Comparator<GroupData>() {
+//            @Override
+//            public int compare(GroupData o1, GroupData o2) {
+//                return Integer.compare(o1.getId(), o2.getId());
+//            }
+//        } ;
+//        int max = after.stream().max(byId).get().getId();
+
+//        Way 3:
+//        Comparator<? super GroupData> byId = (o1, o2) -> Integer.compare(o1.getId(), o2.getId());
+
+        group.setId(after.stream().max((o1, o2) -> Integer.compare(o1.getId(), o2.getId())).get().getId());
         before.add(group);
         Assert.assertEquals(new HashSet<GroupData>(before), new HashSet<GroupData>(after));
     }
