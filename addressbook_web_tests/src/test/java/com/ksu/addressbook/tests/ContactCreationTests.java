85ae5f00360ae65e62f53a721c0fe7f7a6d2e5a0
Ksenia Rybakova <ksuksu1601@gmail.com>
2016-03-27 17:27:20 +0300

    Make contactCreation test parameterized

--- addressbook_web_tests/src/test/java/com/ksu/addressbook/tests/ContactCreationTests.java
+++ addressbook_web_tests/src/test/java/com/ksu/addressbook/tests/ContactCreationTests.java
@@ -1,16 +1,42 @@
 package com.ksu.addressbook.tests;
 
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
 import com.ksu.addressbook.model.ContactData;
 import com.ksu.addressbook.model.Contacts;
 import com.ksu.addressbook.model.GroupData;
 import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.security.Provider;
+import java.util.Iterator;
+import java.util.List;
+import java.util.stream.Collectors;
 
 import static org.hamcrest.CoreMatchers.*;
 import static org.hamcrest.MatcherAssert.*;
 import static org.testng.Assert.assertEquals;
 
 public class ContactCreationTests extends TestBase{
+
+    @DataProvider
+    Iterator<Object[]> validContactsFromJson() throws IOException {
+        BufferedReader reader = new BufferedReader(new FileReader(new File("src/test/resources/contactData.json")));
+        String json = "";
+        String line = reader.readLine();
+        while(line != null){
+            json += line;
+            line = reader.readLine();
+        }
+        Gson gson = new Gson();
+        List<ContactData> contacts = gson.fromJson(json, new TypeToken<List<ContactData>>(){}.getType());
+        return contacts.stream().map((c) -> new Object[]{c}).collect(Collectors.toList()).iterator();
+    }
 
     String groupForContact;
 
@@ -24,16 +50,13 @@ public class ContactCreationTests extends TestBase{
         app.goTo().homePage();
     }
 
-    @Test(enabled = true)
-    public void testContactCreation() {
+    @Test(dataProvider = "validContactsFromJson")
+    public void testContactCreation(ContactData contact) {
         Contacts before = app.contact().all();
-        ContactData contact = new ContactData().withFirstname("Fekla").withLastname("Pupyrkina").withNickname("FeklaP").
-                withAddress("The Mars, 1st street").withFax("000").withMobilePhone("111").withHomePhone("222").
-                withWorkPhone("333").withEmail("fekla.pupyrkina@ino.planet").withGroup(groupForContact);
         app.contact().create(contact);
         assertEquals(app.contact().getContactCount(), before.size() + 1);
         Contacts after = app.contact().all();
-        assertThat(after, equalTo(before.withAdded(contact.withId(after.stream().mapToInt(g -> g.getId()).max().getAsInt()))));
+        assertThat(after, equalTo(before.withAdded(contact.withId(after.stream().mapToInt(c -> c.getId()).max().getAsInt()))));
 
     }
 
