c7fe763ef022ee9a18d8006517b1375d40d3018c
Ksenia Rybakova <ksuksu1601@gmail.com>
2016-03-27 17:42:28 +0300

    Work with files is moved to "try" to close files automatically

--- addressbook_web_tests/src/test/java/com/ksu/addressbook/tests/ContactCreationTests.java
+++ addressbook_web_tests/src/test/java/com/ksu/addressbook/tests/ContactCreationTests.java
@@ -26,16 +26,17 @@ public class ContactCreationTests extends TestBase{
 
     @DataProvider
     Iterator<Object[]> validContactsFromJson() throws IOException {
-        BufferedReader reader = new BufferedReader(new FileReader(new File("src/test/resources/contactData.json")));
-        String json = "";
-        String line = reader.readLine();
-        while(line != null){
-            json += line;
-            line = reader.readLine();
+        try(BufferedReader reader = new BufferedReader(new FileReader(new File("src/test/resources/contactData.json")))){
+            String json = "";
+            String line = reader.readLine();
+            while(line != null){
+                json += line;
+                line = reader.readLine();
+            }
+            Gson gson = new Gson();
+            List<ContactData> contacts = gson.fromJson(json, new TypeToken<List<ContactData>>(){}.getType());
+            return contacts.stream().map((c) -> new Object[]{c}).collect(Collectors.toList()).iterator();
         }
-        Gson gson = new Gson();
-        List<ContactData> contacts = gson.fromJson(json, new TypeToken<List<ContactData>>(){}.getType());
-        return contacts.stream().map((c) -> new Object[]{c}).collect(Collectors.toList()).iterator();
     }
 
     String groupForContact;
