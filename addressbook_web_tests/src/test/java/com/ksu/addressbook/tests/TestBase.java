5ce287e210d39380571f895bce30ee908048e1c5
Ksenia Rybakova <ksuksu1601@gmail.com>
2016-04-02 15:09:13 +0300

    Browser type is defined by system property, setup properties are loaded from .properties file

--- addressbook_web_tests/src/test/java/com/ksu/addressbook/tests/TestBase.java
+++ addressbook_web_tests/src/test/java/com/ksu/addressbook/tests/TestBase.java
@@ -9,7 +9,7 @@ import org.testng.annotations.*;
  */
 public class TestBase {
 
-    protected final static ApplicationManager app = new ApplicationManager(BrowserType.CHROME);
+    protected final static ApplicationManager app = new ApplicationManager(System.getProperty("browser", BrowserType.FIREFOX));
 
     @BeforeSuite
     public void setUp() throws Exception {
