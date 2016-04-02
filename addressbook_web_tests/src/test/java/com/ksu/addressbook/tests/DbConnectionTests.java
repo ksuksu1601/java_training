0ef27bff7b9a02371f291312ef38f3caf1e0d80d
Ksenia Rybakova <ksuksu1601@gmail.com>
2016-04-02 18:50:34 +0300

    Some examples on how to work with DB

---
+++ addressbook_web_tests/src/test/java/com/ksu/addressbook/tests/DbConnectionTests.java
@@ -0,0 +1,73 @@
+package com.ksu.addressbook.tests;
+
+import com.ksu.addressbook.model.GroupData;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.boot.MetadataSources;
+import org.hibernate.boot.registry.StandardServiceRegistry;
+import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+import java.sql.*;
+import java.util.List;
+
+/**
+ * Created by Ksu on 02.04.2016.
+ */
+public class DbConnectionTests {
+
+    private SessionFactory sessionFactory;
+
+    @BeforeClass
+    protected void setUp() throws Exception {
+        // A SessionFactory is set up once for an application!
+        final StandardServiceRegistry registry = new StandardServiceRegistryBuilder()
+                .configure() // configures settings from hibernate.cfg.xml
+                .build();
+        try {
+            sessionFactory = new MetadataSources(registry).buildMetadata().buildSessionFactory();
+        }
+        catch (Exception e) {
+            e.printStackTrace();
+            // The registry would be destroyed by the SessionFactory, but we had trouble building the SessionFactory
+            // so destroy it manually.
+            StandardServiceRegistryBuilder.destroy(registry);
+        }
+    }
+
+    @Test(enabled = false)
+    public void testDbConnection(){
+        Connection conn = null;
+        try {
+            conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/addressbook?user=root&password=");
+            Statement st = conn.createStatement();
+            ResultSet cursor = st.executeQuery("select group_id from group_list");
+
+            while(cursor.next()){
+                System.out.println(cursor.getString("group_id"));
+            }
+
+            cursor.close();
+            st.close();
+            conn.close();
+
+        } catch (SQLException ex) {
+            System.out.println("SQLException: " + ex.getMessage());
+            System.out.println("SQLState: " + ex.getSQLState());
+            System.out.println("VendorError: " + ex.getErrorCode());
+        }
+    }
+
+    @Test
+    public void testHbDbConnection() {
+        Session session = sessionFactory.openSession();
+        session.beginTransaction();
+        List<GroupData> result = session.createQuery("from GroupData").list();
+        for (GroupData group : result) {
+            System.out.println(group);
+        }
+        session.getTransaction().commit();
+        session.close();
+    }
+}
