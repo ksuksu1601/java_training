7b5cb5bbcca86e5466f7635d50a03b6c6ffb7a35
Ksenia Rybakova <ksuksu1601@gmail.com>
2016-04-10 14:29:57 +0300

    New module for mantis tests is added

---
+++ mantis_tests/src/test/java/com/ksu/mantis/tests/TestBase.java
@@ -0,0 +1,25 @@
+package com.ksu.mantis.tests;
+
+import com.ksu.mantis.appmanager.ApplicationManager;
+import org.openqa.selenium.remote.BrowserType;
+import org.testng.annotations.AfterSuite;
+import org.testng.annotations.BeforeSuite;
+
+/**
+ * Created by Ksu on 29.02.2016.
+ */
+public class TestBase {
+
+    protected final static ApplicationManager app = new ApplicationManager(System.getProperty("browser", BrowserType.FIREFOX));
+
+    @BeforeSuite
+    public void setUp() throws Exception {
+        app.init();
+    }
+
+    @AfterSuite(alwaysRun = true)
+    public void tearDown() {
+        app.stop();
+    }
+
+}
